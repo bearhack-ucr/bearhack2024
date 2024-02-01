0ab4cfea8bbb4045ec5cad0625c91aaba100edf2
shahdivyank <divyank.shah.2016@gmail.com>
2024-01-31 19:55:51 -0800

    fix image naming

--- src/data/static/TeamMembers.js
+++ src/data/static/TeamMembers.js
@@ -1,16 +1,16 @@
-import Yogitha from "@/public/board/Yogitha Shastri.png";
-import Sachin from "@/public/board/Sachin Chopra.png";
-import Gagan from "@/public/board/Gagan Mannur.png";
-import Amanda from "@/public/board/Amanda Ly.png";
-import Alex from "@/public/board/Alex Zhang.png";
-import Austin from "@/public/board/Austin Yang.png";
-import Apy from "@/public/board/Apy Sen.png";
-import Rama from "@/public/board/Rama Chunduri.png";
-import Christian from "@/public/board/Christian Macaluso.png";
-import Raidah from "@/public/board/Raidah Fairooz.png";
-import Thomas from "@/public/board/Thomas Li.png";
-import Menthy from "@/public/board/Menthy Wu.png";
-import Divyank from "@/public/board/Divyank Shah.png";
+import Yogitha from "@/public/board/YogithaShastri.png";
+import Sachin from "@/public/board/SachinChopra.png";
+import Gagan from "@/public/board/GaganMannur.png";
+import Amanda from "@/public/board/AmandaLy.png";
+import Alex from "@/public/board/AlexZhang.png";
+import Austin from "@/public/board/AustinYang.png";
+import Apy from "@/public/board/ApySen.png";
+import Rama from "@/public/board/RamaChunduri.png";
+import Christian from "@/public/board/ChristianMacaluso.png";
+import Raidah from "@/public/board/RaidahFairooz.png";
+import Thomas from "@/public/board/ThomasLi.png";
+import Menthy from "@/public/board/MenthyWu.png";
+import Divyank from "@/public/board/DivyankShah.png";
 
 export const directors = [
   {
