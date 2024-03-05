f7a927a7289497b664c92e576aa3e50af4a4fd90
shahdivyank <divyank.shah.2016@gmail.com>
2024-03-04 22:16:10 -0800

    fix tests

--- cypress/component/Checkbox.cy.jsx
+++ cypress/component/Checkbox.cy.jsx
@@ -38,7 +38,7 @@ describe("Checkbox", () => {
 
     cy.mount(<Parent />);
 
-    cy.get('[data-cy="checkbox-bg"]').should("have.class", "bg-bear-teal");
+    cy.get('[data-cy="checkbox-bg"]').should("have.class", "bg-bear-teal/50");
     cy.get('[data-cy="checkmark"]').should("have.class", "text-white");
   });
 
@@ -81,7 +81,7 @@ describe("Checkbox", () => {
     cy.mount(<Parent />);
 
     cy.get('[data-cy="checkbox-bg"]').click();
-    cy.get('[data-cy="checkbox-bg"]').should("have.class", "bg-bear-teal");
+    cy.get('[data-cy="checkbox-bg"]').should("have.class", "bg-bear-teal/50");
     cy.get('[data-cy="checkmark"]').should("have.class", "text-white");
   });
 });
