d3c5594587bc5b6004959c07c1109d945a35cc4c
kellyma073 <kma073@ucr.edu>
2024-04-02 20:00:18 -0700

    dropdown colors

--- src/components/dynamic/Select.jsx
+++ src/components/dynamic/Select.jsx
@@ -63,7 +63,7 @@ const Select = ({
         onClick={() => setShow(!show)}
         className={`${
           !user[field] && "text-hackathon-gray-200"
-        } bg-transparent text-hackathon-gray-200 flex items-center justify-between w-full pb-1`}
+        } bg-transparent flex items-center justify-between w-full pb-1`}
         data-cy="select-toggle"
       >
         {user[field] || placeholder}
