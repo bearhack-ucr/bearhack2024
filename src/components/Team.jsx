9d0007d547aa9f59a759ad57381f5603a533cd94
NovaSagittarii <novasagittarii@gmail.com>
2024-04-15 17:28:20 -0600

    make directors and leads display consistent

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,13 +6,13 @@ const Team = () => {
     <div className="w-full text-white flex flex-col items-center my-14">
       <div className="text-4xl lg:text-5xl font-header">Meet the Team</div>
       <div className="text-2xl lg:text-3xl font-header my-4">Directors</div>
-      <div className="flex w-3/4 flex-wrap justify-center gap-4 md:gap-8">
+      <div className="flex w-3/4 flex-wrap justify-center gap-4 sm:px-4 md:gap-8">
         {directors.map(({ name, position, image }, index) => (
           <Member name={name} position={position} image={image} key={index} />
         ))}
       </div>
       <div className="text-2xl lg:text-3xl font-header my-4">Leads</div>
-      <div className="flex w-3/4 flex-wrap justify-center gap-4 px-4 md:gap-6 md:px-6 lg:gap-8 lg:px-8">
+      <div className="flex w-3/4 flex-wrap justify-center gap-4 sm:px-4 md:gap-6 md:px-6 lg:gap-8 lg:px-8">
         {members.map(({ name, position, image }, index) => (
           <Member name={name} position={position} image={image} key={index} />
         ))}
