00f71eac356815a092776b32316e16a313132f06
Raidah Fairooz <rfair008@ucr.edu>
2024-04-15 03:58:49 -0700

    finished sponsors section with current companies

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import { SPONSORS } from "@/data/Sponsors";
+
 const Sponsors = () => {
-  return <div>Sponsors</div>;
+  return (
+    <div className="w-full justify-center items-center h-full flex flex-col bg-bear-dark mt-20">
+      <div className="font-header text-4xl md:text-5xl text-white flex mb-4 md:mb-20">
+        Sponsors
+      </div>
+      <div className="w-2/3 grid grid-cols-2 grid-rows-2 md:grid-rows-none md:grid-cols-3 gap-12 place-items-center">
+        {SPONSORS.map((item, index) => (
+          <Image key={index} src={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Sponsors;
---
+++ src/data/Sponsors.js
@@ -0,0 +1,9 @@
+import acm from "@/public/acm.webp";
+// import bmes from "@/public/bmes.webp";
+import bmes from "@/public/bmes2.webp";
+// import am from "@/public/am.webp";
+import am from "@/public/am2.webp";
+import numi from "@/public/numi.webp";
+import gcap from "@/public/gcap.webp";
+
+export const SPONSORS = [am, gcap, numi, acm, bmes];
