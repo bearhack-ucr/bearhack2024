f4597f368b852c470ca31536358b3de5d64f5f73
shahdivyank <divyank.shah.2016@gmail.com>
2024-04-22 17:51:03 -0700

    fix email

--- src/utils/email.js
+++ src/utils/email.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const send = async ({ email, id, name, position, subject, preview }) => {
   const { data, error } = await resend.emails.send({
     from: "Bearhack <info@bearhack.com>",
-    to: ["bearhackucr@gmail.com"],
+    to: [email],
     subject: subject,
     // eslint-disable-next-line new-cap
     react: Email({ id, name, position, preview }),
