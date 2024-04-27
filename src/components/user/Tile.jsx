673f55d0a6990889988765e30d63809bc2e8e2c3
shahdivyank <divyank.shah.2016@gmail.com>
2024-04-27 01:31:01 -0700

    responsive

--- src/components/user/Tile.jsx
+++ src/components/user/Tile.jsx
@@ -4,7 +4,7 @@ const Tile = ({ icon, text, link }) => {
   return (
     <Link
       href={link}
-      className="bg-white w-1/2 rounded-xl p-7 gap-5 shadow-xl flex items-center hover:opacity-90"
+      className="bg-white w-full rounded-xl p-7 gap-5 shadow-xl flex items-center hover:opacity-90"
     >
       <div className="text-black text-7xl">{icon}</div>
       <div className="text-black font-bold text-2xl leading-9">{text}</div>
