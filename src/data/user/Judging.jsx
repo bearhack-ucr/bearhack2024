8fc6441bbc0ede3e91eacd90a55641ab3a7d0a8a
menthy-wu <wumengxuan0728@gmail.com>
2024-04-26 21:19:21 -0700

    fix user dashbaord

--- src/data/user/Judging.jsx
+++ src/data/user/Judging.jsx
@@ -1,8 +1,5 @@
 export const JUDGING = [
-  "You may not submit a project that has already been submitted or started before our hackathon period.",
-  "Your project must be an original and not copied from someone else's work.",
-  "Teams of 1-4 are required.",
-  "A Github is required for submission.",
-  "Github commits may not be dated before the hackathon period has started.",
-  "We will not judge any late projects.",
+  "Please submit your Devpost link on the hacker portal to be considered for judging.",
+  "Make sure to put your github link in the Devpost submission.",
+  "Please make sure all the links are public",
 ];
