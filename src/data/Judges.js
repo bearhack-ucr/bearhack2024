90bf3c8f870c22b107d3a019c4a44275ff48de4c
ACM UCR <fake@domain.com>
2024-04-27 04:20:07 +0000

    convert images to webp

--- src/data/Judges.js
+++ src/data/Judges.js
@@ -1,12 +1,12 @@
-import RichYueh from "@/public/judges/RichYueh.jpg";
-import EthanRoos from "@/public/judges/EthanRoos.jpg";
-import RohitManimaran from "@/public/judges/RohitManimaran.jpg";
-import KaranBhogal from "@/public/judges/KaranBhogal.jpg";
-import LisaChen from "@/public/judges/LisaChen.jpg";
-import JoshuaCandelaria from "@/public/judges/JoshuaCandelaria.jpg";
-import HalliePham from "@/public/judges/HalliePham.jpg";
-import RachitPrajapati from "@/public/judges/RachitPrajapati.jpg";
-import SamarthSrinivasa from "@/public/judges/SamarthSrinivasa.jpg";
+import RichYueh from "@/public/judges/RichYueh.webp";
+import EthanRoos from "@/public/judges/EthanRoos.webp";
+import RohitManimaran from "@/public/judges/RohitManimaran.webp";
+import KaranBhogal from "@/public/judges/KaranBhogal.webp";
+import LisaChen from "@/public/judges/LisaChen.webp";
+import JoshuaCandelaria from "@/public/judges/JoshuaCandelaria.webp";
+import HalliePham from "@/public/judges/HalliePham.webp";
+import RachitPrajapati from "@/public/judges/RachitPrajapati.webp";
+import SamarthSrinivasa from "@/public/judges/SamarthSrinivasa.webp";
 
 export const judges = [
   {
