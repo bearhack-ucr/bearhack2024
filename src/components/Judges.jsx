fa6a35f655afd1fc526e9a8c8ec7fe9c1f90b016
NovaSagittarii <novasagittarii@gmail.com>
2024-04-26 22:25:25 -0600

    use 1 col for judges on mobile
    otherwise it's uneven due to long titles

--- src/components/Judges.jsx
+++ src/components/Judges.jsx
@@ -3,9 +3,9 @@ import Member from "@/components/Member";
 
 const Judges = () => {
   return (
-    <div className="w-full text-white flex flex-col items-center my-14">
+    <div className="w-full text-white flex flex-col items-center my-14 gap-16">
       <div className="text-4xl lg:text-5xl font-header">Judges</div>
-      <div className="flex w-3/4 flex-wrap justify-center gap-4 sm:px-4 md:gap-8">
+      <div className="grid grid-cols-1 sm:flex w-3/4 flex-wrap justify-center gap-8 sm:px-4">
         {judges.map(({ name, position, image }, index) => (
           <Member name={name} position={position} image={image} key={index} />
         ))}
