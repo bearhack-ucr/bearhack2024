61a124bbd95c5438f84d87d478dd642f6512a7c6
shahdivyank <divyank.shah.2016@gmail.com>
2024-04-30 13:17:37 -0700

    finalize postlive

--- src/data/Releases.js
+++ src/data/Releases.js
@@ -1,10 +1,10 @@
 const DEV = {
   LIVE: {
     START: new Date("10/01/2000"),
-    END: new Date("April 29, 2024 9:00:00"),
+    END: new Date("April 28, 2025 9:00:00"),
   },
   POSTLIVE: {
-    START: new Date("April 29, 2024 9:00:00"),
+    START: new Date("April 28, 2024 9:00:00"),
     END: new Date("10/10/2100"),
   },
 
@@ -24,29 +24,29 @@ const DEV = {
   "/admin/judging": new Date("10/01/2023"),
   "/admin/statistics": new Date("10/01/2023"),
 
-  "/form/participant": new Date("10/01/2023"),
-  "/form/judge": new Date("10/01/2023"),
-  "/form/volunteer": new Date("10/01/2023"),
-  "/form/mentor": new Date("10/01/2023"),
-  "/form/admin": new Date("10/01/2023"),
-  "/form/committee": new Date("10/01/2023"),
-  "/form/sponsor": new Date("10/01/2023"),
-  "/form/panel": new Date("10/01/2023"),
+  "/form/participant": new Date("10/01/2025"),
+  "/form/judge": new Date("10/01/2025"),
+  "/form/volunteer": new Date("10/01/2025"),
+  "/form/mentor": new Date("10/01/2025"),
+  "/form/admin": new Date("10/01/2025"),
+  "/form/committee": new Date("10/01/2025"),
+  "/form/sponsor": new Date("10/01/2025"),
+  "/form/panel": new Date("10/01/2025"),
   "/form/feedback": new Date("10/01/2023"),
-  "/form/interest": new Date("10/01/2023"),
+  "/form/interest": new Date("10/01/2025"),
 
-  "/user/checkin": new Date("10/01/2023"),
-  "/user/dashboard": new Date("10/01/2023"),
-  "/user/join": new Date("10/01/2023"),
+  "/user/checkin": new Date("10/01/2025"),
+  "/user/dashboard": new Date("10/01/2025"),
+  "/user/join": new Date("10/01/2025"),
 };
 
 const PRODUCTION = {
   LIVE: {
     START: new Date("10/01/2023"),
-    END: new Date("April 29, 2024 9:00:00"),
+    END: new Date("April 28, 2024 9:00:00"),
   },
   POSTLIVE: {
-    START: new Date("April 29, 2024 9:00:00"),
+    START: new Date("April 28, 2024 9:00:00"),
     END: new Date("10/10/2100"),
   },
   "/admin/participants": new Date("10/01/2023"),
@@ -65,20 +65,20 @@ const PRODUCTION = {
   "/admin/judging": new Date("10/01/2023"),
   "/admin/statistics": new Date("10/01/2023"),
 
-  "/form/participant": new Date("10/01/2023"),
-  "/form/judge": new Date("10/01/2023"),
-  "/form/volunteer": new Date("10/01/2023"),
-  "/form/mentor": new Date("10/01/2023"),
-  "/form/admin": new Date("10/01/2023"),
-  "/form/committee": new Date("10/01/2023"),
-  "/form/sponsor": new Date("10/01/2023"),
-  "/form/panel": new Date("10/01/2023"),
+  "/form/participant": new Date("10/01/2025"),
+  "/form/judge": new Date("10/01/2025"),
+  "/form/volunteer": new Date("10/01/2025"),
+  "/form/mentor": new Date("10/01/2025"),
+  "/form/admin": new Date("10/01/2025"),
+  "/form/committee": new Date("10/01/2025"),
+  "/form/sponsor": new Date("10/01/2025"),
+  "/form/panel": new Date("10/01/2025"),
   "/form/feedback": new Date("10/01/2023"),
-  "/form/interest": new Date("10/01/2023"),
+  "/form/interest": new Date("10/01/2025"),
 
-  "/user/checkin": new Date("4/20/2024"),
-  "/user/dashboard": new Date("4/20/2024"),
-  "/user/join": new Date("4/20/2024"),
+  "/user/checkin": new Date("4/20/2025"),
+  "/user/dashboard": new Date("4/20/2025"),
+  "/user/join": new Date("4/20/2025"),
 };
 
 export default process.env.NODE_ENV === "production" ? PRODUCTION : DEV;
